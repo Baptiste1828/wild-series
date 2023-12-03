acfb355f7b5c2315fea04d69f4351ab9b04d8ed3
Baptiste Delefosse <baptiste.delefosse@hotmail.fr>
2023-12-03 16:59:55 +0100

    Quest12: done

--- src/DataFixtures/SeasonFixtures.php
+++ src/DataFixtures/SeasonFixtures.php
@@ -6,27 +6,32 @@ use App\Entity\Season;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory as Faker;
 
 class SeasonFixtures extends Fixture implements DependentFixtureInterface
 {
     const DATAS = [
-        [2023, 'First saison of The Last of Us', 'The_last_of_Us'],
-        [2010, 'First saison of Walking dead', 'Walking_dead'],
-        [2019, 'First saison of The Boys', 'The_Boys'],
-        [2022, 'First saison of The Rings of Power', 'The_Rings_of_Power'],
-        [2023, 'First saison of D\'argent et de sang', 'D\'argent_et_de_sang'],
+        'The_last_of_Us',
+        'Walking_dead',
+        'The_Boys',
+        'The_Rings_of_Power',
+        'D\'argent_et_de_sang',
     ];
 
     public function load(ObjectManager $manager): void
     {
+        $faker = Faker::create();
+
         foreach (self::DATAS as $data) {
-            $season = new Season();
-            $season->setNumber(1);
-            $season->setYear($data[0]);
-            $season->setDescription($data[1]);
-            $season->setProgram($this->getReference('program_' . $data[2]));
-            $manager->persist($season);
-            $this->addReference('saison1_' . $data[2], $season);
+            for ($i = 1; $i < 6; $i++) {
+                $season = new Season();
+                $season->setNumber($i);
+                $season->setYear($faker->year());
+                $season->setDescription($faker->text());
+                $season->setProgram($this->getReference('program_' . $data));
+                $manager->persist($season);
+                $this->addReference('saison' . $i . '_' . $data, $season);
+            }
         }
 
         $manager->flush();
